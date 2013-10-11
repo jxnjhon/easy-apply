6ce2d6489d4aa278de56025a6903eb52c35c69bb
Faris Mohammed <farismosman@gmail.com>
2013-10-11 09:27:36 +0300

    [Faris] - Created the template.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyApply::Application.config.secret_token = '363f4c1233e610c76fa2993d9889d0f980f54ffbcfb9bf84f24b2f86336b0ecdca254fb5fe27318262a15497f0e82e272ee137eac43eb124c4bcb68636e1bd3c'
